48060c4ea2032f3d4f4d2d7794be7eda02d40fb9
Andrew Davies <andy.marinebio@googlemail.com>
2019-02-15 15:18:40 -0500

    Update Step_0.py

--- Classes/5_arcpy/Step_0.py
+++ Classes/5_arcpy/Step_0.py
@@ -7,15 +7,21 @@
 
 ##### Create an integer list 1, 2, 3, 4, 5, 6, 7, call it "intList"
 
-##### Create a string list using the sentence: "EvEryoNe waS buSy SO I WenT To THE MOVie aLOne" and call it "strList"
+
+##### Create a string list using the sentence: "daYS lAtEr EvEryoNe waS buSy SO I WenT To THE MOVie aLOne" and call it "strList"
+
 
 # Task b: Using a for loop, do the following:
 
-##### Sum the values of intList and print result (28), yes you can use: print sum(intList), but humor me.
+##### Sum the values of intList and print result (28), yes you can use: print sum(intList), call it strValue.
 
-##### Go through each word of strList, and create a sentence where each word is in lower case ( i.e.*.lower() )
+
+
+##### Go through each word of strList, and create a sentence (called strSentence) where each word is in lower case ( i.e.*.lower() )
+
 
 ##### Concatenate (join) the result of strValue and strSentence:
+
 
 # Task c: Use if to check status:
 
