e065072584a2c97bb08f79a2bd85311fa3befe62
Andrew Davies <andy.marinebio@googlemail.com>
2020-02-25 17:11:48 -0500

    Added Class 6

---
+++ Classes/6_Cheating/Step_0.py
@@ -0,0 +1,28 @@
+
+#####
+# Step 0 - Practice tasks before we start.
+#####
+
+# Express this string of input files as a list, call the resulting list listFiles and print it:
+
+stringFiles = "file1.asc;file2.asc;file3.asc;file4.asc;file5.asc"
+
+
+# How many files are in the list?
+
+
+# Take this list of files (listFiles), and using a for loop, go through each file name and print it.
+
+
+# Now, using the code below, create those files, and add a small piece of text (this is my file) to each file:
+# file_create = open(**FILENAME**, "w")
+# file_create.write("This is my file\n")
+
+
+# Now do the opposite, read the contents of each file and print it, here is some example code:
+# file = open("testfile.txt", "r")
+# file_contents = file.read()
+# print (file_contents)
+# file.close()
+
+
