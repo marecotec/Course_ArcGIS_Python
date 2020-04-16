5b48c024c0f31820b7a2b59cd20686220820ad76
Andrew Davies <andy.marinebio@googlemail.com>
2020-04-16 19:23:53 -0400

    Added class 10 rasters

---
+++ Classes/10_Rasters/Step_1_Answers.py
@@ -0,0 +1,36 @@
+
+#####
+# Step 1 - Zonal stats and extracting values to points
+#####
+
+# Raster processing in arcpy is very similar to working with shapefiles and other feature classes. In this example, we
+# conduct some zonal statistics and undertake point value extraction from a raster.
+
+# Using Step_1_Data.zip, extract teh zonal values for the shapefile: Biogeography_Made_Up.shp, and the sst_mean.tif
+# raster.
+import arcpy
+arcpy.CheckOutExtension("Spatial")
+arcpy.env.overwriteOutput = True
+arcpy.env.workspace = r"Z:\Andy's Documents\Teaching and students\URI\NRS - GIS Python Course\Github\Course_ArcGIS_Python\Classes\12_Rasters\Step_1_Data"
+arcpy.gp.ZonalStatisticsAsTable_sa("Biogeography_Made_Up.shp", "Area", "sst_mean.tif", "sst_mean_zones.dbf", "DATA", "ALL")
+
+# Task 1 -  Using a for loop, process all three *.tif files for SST (mean, min and max), you will need to edit the code above.
+# HInt: ZonalStats... is sensitive to file name output, you may need to use something that shortebs the output table name:
+# e.g. file_name = raster[:6] + "_out.dbf"
+
+raster_list = arcpy.ListRasters()
+
+for raster in raster_list:
+    file_name = raster[:6] + "_out.dbf"
+    arcpy.gp.ZonalStatisticsAsTable_sa("Biogeography_Made_Up.shp", "Area", raster, file_name, "DATA",
+                                       "ALL")
+
+# Pulling values from a raster using a point shapefule is also pretty easy, this time, we use the Extract Values tool.
+
+arcpy.gp.ExtractValuesToPoints_sa("Great_Whites.shp", "sst_mean.tif", "Great_Whites_Extract.shp", "NONE", "VALUE_ONLY")
+
+# Task 2 - I want you to run the tool above on the rasters provided (mean, min and max). Think about how you will do this,
+# maybe search online about extracting multiple values to points.
+
+raster_list = arcpy.ListRasters()
+arcpy.gp.ExtractMultiValuesToPoints("Great_Whites.shp", raster_list, "BILINEAR")
---
+++ Classes/10_Rasters/Step_3_Answers.py
@@ -0,0 +1,55 @@
+
+#####
+# Step 3 - Rasters and numpy
+#####
+#
+# # Numpy is a exceptionally powerful package for Python that gives incredible scientific computing functionality.
+# # Some basic functionality has been built into arcpy that allows us to interact with it in a fairly seamless manner.
+#
+# # Review the code below, this is a simple conversion that we are doing, converting Meters to Feet in an elevation
+# # raster. Of course you could do this using Raster Calculator, but what if you want to do more complex statistics,
+# # model simulations and so on?
+#
+# # Be aware that the size of the raster is crucial as the resulting numpy array will reside in your computer
+# # memory, so no 10gb files!
+#
+# import arcpy
+#
+# arcpy.env.overwriteOutput = True
+# arcpy.env.workspace = r"Z:\Andy's Documents\Teaching and students\URI\NRS - GIS Python Course\Github\Course_ArcGIS_Python\Classes\12_Rasters\Step_3_Data"
+# inRas = arcpy.Raster("etopo10")
+# lowerLeft = arcpy.Point(inRas.extent.XMin,inRas.extent.YMin)
+# cellSize = inRas.meanCellWidth
+#
+# arr = arcpy.RasterToNumPyArray(inRas,nodata_to_value=0)
+#
+# # Print the resulting array
+# print arr.shape
+#
+# # Now lets convert m to feet
+# arrFeet = arr * 3.28084
+#
+# newRaster = arcpy.NumPyArrayToRaster(arrFeet, lowerLeft, cellSize, value_to_nodata=0)
+# newRaster.save("etopo10_ft.tif")
+#
+#
+# # Task: Using the etopo10 dataset and a NumPyArray, extract only land values, replacing them with null data. Hint:
+# # arr[arr < 0] = -9999
+
+import arcpy
+
+arcpy.env.workspace = r"C:\Course_ArcGIS_Python\Classes\10_Rasters\Step_3_Data"
+inRas = arcpy.Raster("etopo10")
+lowerLeft = arcpy.Point(inRas.extent.XMin,inRas.extent.YMin)
+cellSize = inRas.meanCellWidth
+
+arr = arcpy.RasterToNumPyArray(inRas,nodata_to_value=0)
+
+# Print the resulting array
+print arr.shape
+
+# Now lets convert m to feet
+arr[arr < 0] = -9999
+
+newRaster = arcpy.NumPyArrayToRaster(arr, lowerLeft, cellSize, value_to_nodata=-9999)
+newRaster.save("etopo10_land.tif")
